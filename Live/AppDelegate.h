94d21c95b124fe9ca05fdfa99703e46159c35270
Cheng <Cheng@liruideiMac.local>
2017-05-05 16:47:12 +0800

    first commit

---
+++ Live/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Live
+//
+//  Created by Cheng on 2017/5/5.
+//  Copyright © 2017年 Joe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
