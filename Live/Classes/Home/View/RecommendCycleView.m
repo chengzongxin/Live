8492a47326cffd551564bb6180a21a3f77e65699
chengzongxin <8876142@qq.com>
2017-05-10 18:01:04 +0800

    添加计时器自动滚动

--- Live/Classes/Home/View/RecommendCycleView.m
+++ Live/Classes/Home/View/RecommendCycleView.m
@@ -10,11 +10,13 @@
 #import "CycleModel.h"
 #import "CollectionCycleCell.h"
 #define kCycleCellID @"kCycleCellID"
-@interface RecommendCycleView () <UICollectionViewDataSource>
+@interface RecommendCycleView () <UICollectionViewDataSource,UICollectionViewDelegate>
 {
     NSArray *_cycleArray;
+    NSTimer *_cycleTimer;
 }
 @property (nonatomic,retain) NSArray *cycleArray;
+@property (nonatomic,retain) NSTimer *cycleTimer;
 @end
 
 @implementation RecommendCycleView
@@ -24,6 +26,8 @@
 //    self.autoresizingMask = UIViewAutoresizingNone; 设置不自动拉伸，否则cycleview不能显示
     
     [_collectionView registerNib:[UINib nibWithNibName:@"CollectionCycleCell" bundle:nil] forCellWithReuseIdentifier:kCycleCellID];
+    
+    [self addCycleTimer];
 }
 
 - (void)reloadDataWith:(NSArray *)cycleArray
@@ -31,6 +35,8 @@
     self.cycleArray = cycleArray;
     [_collectionView reloadData];
     _pageControl.numberOfPages = cycleArray.count;
+    // 默认滚动到60处，那么用户向前滚动也有内容
+    [_collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForItem:self.cycleArray.count * 10 inSection:0] atScrollPosition:UICollectionViewScrollPositionLeft animated:NO];
 }
 
 // 布局的尺寸必须在layoutsubviews中计算，否则拿到的size不准确
@@ -48,7 +54,7 @@
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return self.cycleArray.count;
+    return self.cycleArray.count * 1000;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
@@ -57,9 +63,52 @@
     
     cell = [collectionView dequeueReusableCellWithReuseIdentifier:kCycleCellID forIndexPath:indexPath];
     
-    cell.cycleModel = self.cycleArray[indexPath.item];
+    cell.cycleModel = self.cycleArray[indexPath.item%self.cycleArray.count];
     
     return cell;
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    CGFloat offset = scrollView.contentOffset.x + scrollView.bounds.size.width * 0.5;  // 拖动到一半就显示到下一个
+    
+    int offsetIndex = (int)offset / scrollView.bounds.size.width;
+    
+    _pageControl.currentPage = offsetIndex % self.cycleArray.count;
+}
+
+
+#pragma mark -- 定时器自动轮播
+- (void)scrollToNext
+{
+    CGFloat offset = _collectionView.contentOffset.x + _collectionView.bounds.size.width;
+    [_collectionView setContentOffset:CGPointMake(offset, 0) animated:YES];
+}
+
+- (void)addCycleTimer
+{
+    _cycleTimer = [NSTimer timerWithTimeInterval:3.0 target:self selector:@selector(scrollToNext) userInfo:nil repeats:YES];
+    
+    [[NSRunLoop mainRunLoop] addTimer:_cycleTimer forMode:NSRunLoopCommonModes];
+}
+
+- (void)removeCycleTime
+{
+    [_cycleTimer invalidate];
+    
+    _cycleTimer = nil;
+}
+
+
+// 用户拖拽过程中，定时器不更新
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
+{
+    [self removeCycleTime];
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+{
+    [self addCycleTimer];
 }
 
 @end
